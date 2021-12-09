9774d5d390a2fb9a9bf4aff46b7cdee9211e5970
Tom Mason <masonworldt@gmail.com>
2021-12-09 15:51:21 +0000

    removed deprecated connectFour

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -109,276 +109,7 @@ class BoardTest {
     }
 
     @Test
-    void test_connectFour_Horizontal() throws ColumnFullException {
-
-        var board = new Board();
-
-        board.set(0, 'r');
-        board.set(1, 'r');
-        board.set(2, 'r');
-        board.set(3, 'r');
-
-        assert(board.connectFour());
-
-        board = new Board();
-
-        board.set(3, 'y');
-        board.set(4, 'y');
-        board.set(5, 'y');
-        board.set(6, 'y');
-
-        assert(board.connectFour());
-
-        board = new Board();
-
-        board.set(0, 'r');
-        board.set(1, 'y');
-        board.set(2, 'r');
-        board.set(3, 'r');
-
-        assert(!board.connectFour());
-
-        board = new Board();
-
-        board.set(0, 'r');
-        board.set(0, 'r');
-
-        board.set(4, 'r');
-
-        board.set(5, 'r');
-
-        board.set(6, 'r');
-
-        assert(!board.connectFour());
-    }
-
-    @Test
-    void test_connectFour_Vertical() throws ColumnFullException {
-
-        var board = new Board();
-
-        board.set(0, 'r');
-        board.set(0, 'r');
-        board.set(0, 'r');
-        board.set(0, 'r');
-
-        assert(board.connectFour());
-
-        board = new Board();
-
-        board.set(6, 'y');
-        board.set(6, 'y');
-        board.set(6, 'y');
-        board.set(6, 'y');
-
-        assert(board.connectFour());
-
-        board = new Board();
-
-        board.set(0, 'r');
-        board.set(0, 'y');
-        board.set(0, 'r');
-        board.set(0, 'r');
-
-        assert(!board.connectFour());
-
-        board = new Board();
-
-        board.set(0, 'a');
-        board.set(0, 'b');
-        board.set(0, 'c');
-        board.set(0, 'r');
-        board.set(0, 'r');
-        board.set(0, 'r');
-
-        board.set(1, 'r');
-
-        assert(!board.connectFour());
-    }
-
-    @Test
-    void test_connectFour_Diagonal_BotLeft_To_TopRight() throws ColumnFullException {
-
-        var board = new Board();
-
-        board.set(0, 'r');
-        board.set(1, 'y');
-        board.set(1, 'r');
-        board.set(2, 'y');
-        board.set(2, 'y');
-        board.set(2, 'r');
-        board.set(3, 'y');
-        board.set(3, 'y');
-        board.set(3, 'y');
-        board.set(3, 'r');
-
-        assert(board.connectFour());
-
-        board = new Board();
-
-        board.set(3, 'y');
-
-        board.set(4, 'a');
-        board.set(4, 'y');
-
-        board.set(5, 'b');
-        board.set(5, 'c');
-        board.set(5, 'y');
-
-        board.set(6, 'd');
-        board.set(6, 'e');
-        board.set(6, 'f');
-        board.set(6, 'y');
-
-        assert(board.connectFour());
-
-        board = new Board();
-
-        board.set(0, 'a');
-        board.set(0, 'b');
-        board.set(0, 'y');
-
-        board.set(1, 'c');
-        board.set(1, 'd');
-        board.set(1, 'e');
-        board.set(1, 'y');
-
-        board.set(2, 'f');
-        board.set(2, 'g');
-        board.set(2, 'h');
-        board.set(2, 'i');
-        board.set(2, 'y');
-
-        board.set(3, 'j');
-        board.set(3, 'k');
-        board.set(3, 'l');
-        board.set(3, 'm');
-        board.set(3, 'n');
-        board.set(3, 'y');
-
-        assert(board.connectFour());
-
-        board = new Board();
-
-        board.set(3, 'a');
-        board.set(3, 'b');
-        board.set(3, 'y');
-
-        board.set(4, 'c');
-        board.set(4, 'd');
-        board.set(4, 'e');
-        board.set(4, 'y');
-
-        board.set(5, 'f');
-        board.set(5, 'g');
-        board.set(5, 'h');
-        board.set(5, 'i');
-        board.set(5, 'y');
-
-        board.set(6, 'j');
-        board.set(6, 'k');
-        board.set(6, 'l');
-        board.set(6, 'm');
-        board.set(6, 'n');
-        board.set(6, 'y');
-
-        assert(board.connectFour());
-    }
-
-    @Test
-    void test_connectFour_Diagonal_BotRight_To_TopLeft() throws ColumnFullException {
-
-        var board = new Board();
-
-        board.set(3, 'r');
-
-        board.set(2, 'a');
-        board.set(2, 'r');
-
-        board.set(1, 'b');
-        board.set(1, 'c');
-        board.set(1, 'r');
-
-        board.set(0, 'd');
-        board.set(0, 'e');
-        board.set(0, 'f');
-        board.set(0, 'r');
-
-        assert(board.connectFour());
-
-        board = new Board();
-
-        board.set(3, 'r');
-
-        board.set(4, 'a');
-        board.set(4, 'r');
-
-        board.set(5, 'b');
-        board.set(5, 'c');
-        board.set(5, 'r');
-
-        board.set(6, 'd');
-        board.set(6, 'e');
-        board.set(6, 'f');
-        board.set(6, 'r');
-
-        assert(board.connectFour());
-
-        board = new Board();
-
-        board.set(3, 'a');
-        board.set(3, 'b');
-        board.set(3, 'r');
-
-        board.set(2, 'c');
-        board.set(2, 'd');
-        board.set(2, 'e');
-        board.set(2, 'r');
-
-        board.set(1, 'f');
-        board.set(1, 'g');
-        board.set(1, 'h');
-        board.set(1, 'i');
-        board.set(1, 'r');
-
-        board.set(0, 'j');
-        board.set(0, 'k');
-        board.set(0, 'l');
-        board.set(0, 'm');
-        board.set(0, 'n');
-        board.set(0, 'r');
-
-        assert(board.connectFour());
-
-        board = new Board();
-
-        board.set(6, 'a');
-        board.set(6, 'b');
-        board.set(6, 'r');
-
-        board.set(5, 'c');
-        board.set(5, 'd');
-        board.set(5, 'e');
-        board.set(5, 'r');
-
-        board.set(4, 'f');
-        board.set(4, 'g');
-        board.set(4, 'h');
-        board.set(4, 'i');
-        board.set(4, 'r');
-
-        board.set(3, 'j');
-        board.set(3, 'k');
-        board.set(3, 'l');
-        board.set(3, 'm');
-        board.set(3, 'n');
-        board.set(3, 'r');
-
-        assert(board.connectFour());
-    }
-
-    @Test
-    void test_indexedConnectFour_Horizontal() throws ColumnFullException {
+    void test_ConnectFour_Horizontal() throws ColumnFullException {
 
         var board = new Board();
 
@@ -431,7 +162,7 @@ class BoardTest {
     }
 
     @Test
-    void test_indexedConnectFour_Vertical() throws ColumnFullException {
+    void test_ConnectFour_Vertical() throws ColumnFullException {
 
         var board = new Board();
 
@@ -475,7 +206,7 @@ class BoardTest {
     }
 
     @Test
-    void test_indexedConnectFour_Diagonal_BotLeft_To_TopRight() throws ColumnFullException {
+    void test_ConnectFour_Diagonal_BotLeft_To_TopRight() throws ColumnFullException {
 
         var board = new Board();
 
@@ -576,7 +307,7 @@ class BoardTest {
     }
 
     @Test
-    void test_indexedConnectFour_Diagonal_BotRight_To_TopLeft() throws ColumnFullException {
+    void test_ConnectFour_Diagonal_BotRight_To_TopLeft() throws ColumnFullException {
 
         var board = new Board();
 
